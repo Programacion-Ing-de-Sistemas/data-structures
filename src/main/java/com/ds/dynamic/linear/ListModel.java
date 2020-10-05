70ac03e4fd537fda7ca26c47c23c9132994834bc
Jhon Samuel <jhon.fernandez.27@unsch.edu.pe>
2020-10-05 18:25:41 -0500

    Auto fill
    Added to lists the ability to get items from other lists.

--- src/main/java/com/ds/dynamic/linear/ListModel.java
+++ src/main/java/com/ds/dynamic/linear/ListModel.java
@@ -12,6 +12,14 @@ public interface ListModel<T> {
 
     void remove(int position);
 
+    default void fill(ListModel<T> list) {
+        if (list != null) {
+			for (int i = 0; i < list.size(); i++) {
+				this.addEnd(list.get(i));
+			}
+		}
+    }
+
     T get(int position);
 
     void clear();
